c4133d0e18d51116ed821973e36a8801d8cf0492
Vedant Patel <vedantpatel1827@gmail.com>
2024-01-22 21:49:12 +0530

    5th commit front-end

--- src/Components/Body/Note/Addnote.js
+++ src/Components/Body/Note/Addnote.js
@@ -62,31 +62,36 @@ export default function Addnote({state,set,fetchdata}) {
       };
 
     return(
-      <div className="sm:px-20 sm:w-full md:w-full lg:w-full xl:w-full h-full m-0 p-0 top-0 bottom-0 bg-[#d4d6d644] absolute" style={{ display: state ? "block" : "none" }}>
-  <div className="sm:w-11/12 md:w-10/12 lg:w-8/12 xl:w-6/12 sm:h-[70vh] md:h-[80vh] lg:h-[90vh] xl:h-[90vh] bg-white fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 rounded-md">
-    <div className=" head flex justify-between text-base font-semibold font-sans text-black-500">
-      <div>ADD NOTE</div>
-      <button onClick={set}><i className="fa-solid fa-x font-bold hover:text-red-600"></i></button>
+      <div className="sm:w-full md:w-full lg:w-full xl:w-full h-full m-0 p-0 top-0 bottom-0 bg-[#d4d6d644] absolute" style={{ display: state ? "block" : "none" }}>
+      <div className="sm:w-full md:w-10/12 lg:w-8/12 xl:w-6/12 sm:h-[80vh] md:h-[80vh] lg:h-[80vh] xl:h-[80vh] bg-white fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 rounded-md">
+        <div className="head flex justify-between text-base font-semibold font-sans text-black-500">
+          <div>ADD NOTE</div>
+          <button onClick={set}><i className="fa-solid fa-x font-bold hover:text-red-600"></i></button>
+        </div>
+        <hr className="mt-2" />
+        <form onSubmit={handleSubmit} className="mt-2">
+          <label htmlFor="" className="font-semibold text-lg mt-2 block"> Date :</label>
+          <input type="date" name="date" id="date" defaultValue={formData.date} onChange={handleChange} required />
+          
+          <label htmlFor="" className="font-semibold text-lg mt-2 block">Type : </label>
+          <select name="type" id="type" defaultValue={formData.type} onChange={handleChange} required className="w-full border rounded py-2 px-3 mt-1">
+            <option defaultValue="Select">__Select__</option>
+            <option defaultValue="Project">Project</option>
+            <option defaultValue="Personal">Personal</option>
+            <option defaultValue="Business">Business</option>
+          </select>
+          
+          <label htmlFor="" className="font-semibold text-lg mt-2 block">Title:</label>
+          <input type="text" name="title" id="title" placeholder=" Enter Title .." defaultValue={formData.title} onChange={handleChange} required className="w-full border rounded py-2 px-3 mt-1" />
+          
+          <label htmlFor="" className="font-semibold text-lg mt-2 block">Description:</label>
+          <textarea name="para" id="para" cols="60" rows="5" placeholder="Enter Description..." defaultValue={formData.para} onChange={handleChange} required className="w-full border rounded py-2 px-3 mt-1"></textarea>
+          
+          <button type="submit" className="px-5 text-base font-semibold font-sans text-white rounded-md p-1 bg-[#8981D8] mt-5">ADD</button>
+        </form>
+      </div>
     </div>
-    <hr className="mt-2" />
-    <form onSubmit={handleSubmit} className="mt-2">
-      <label htmlFor="" className="font-semibold text-lg mt-2"> Date :</label>
-      <input type="date" name="date" id="date" defaultValue={formData.date} onChange={handleChange} required />  <br />
-      <label htmlFor="" className="font-semibold text-lg mt-2">Type : </label>
-      <select name="type" id="type" defaultValue={formData.type} onChange={handleChange} required>
-        <option defaultValue="Select">__Select__</option>
-        <option defaultValue="Project">Project</option>
-        <option defaultValue="Personal">Personal</option>
-        <option defaultValue="Business">Business</option>
-      </select> <br />
-      <label htmlFor="" className="font-semibold text-lg mt-2">Title:</label>
-      <input type="text" name="title" id="title" placeholder=" Enter Title .." defaultValue={formData.title} onChange={handleChange} required /> <br />
-      <label htmlFor="" className="font-semibold text-lg mt-2">Description:</label>
-      <textarea name="para" id="para" cols="60" rows="5" placeholder="Enter Description..." defaultValue={formData.para} onChange={handleChange} required></textarea> <br />
-      <button type="submit" className="px-5 text-base font-semibold font-sans text-white rounded-md p-1 bg-[#8981D8] mt-5">ADD</button>
-    </form>
-  </div>
-</div>
+    
 
     
   
